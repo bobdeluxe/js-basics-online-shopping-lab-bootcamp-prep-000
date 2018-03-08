9b7881adbbf35d11260a5771f288c0e06ce692f4
bob deluxe <learncobobdeluxe@gmail.com>
2018-03-08 20:42:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ function placeOrder(cardNumber) {
   if(cardNumber===undefined) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    var total = total()
+    var balance = total()
     var cart = []
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${balance}, which will be charged to the card ${cardNumber}.`
   }
 }
