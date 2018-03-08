bd672d8a13c0a3726265bd1c19ffdf09378d5d5c
bob deluxe <learncobobdeluxe@gmail.com>
2018-03-08 20:05:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ function addToCart(item) {
 
 function viewCart() {
   if(cart.length===0) {
-    return ""
+    return "Your shopping cart is empty."
+  }
+  if(cart.length===1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }
+  if(cart.length===2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice} and ${cart[1].itemName} at $${cart[1].itemPrice}.`
   }
 }
 
